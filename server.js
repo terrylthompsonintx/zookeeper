be6cbc854fa94159b8ee44b04b9504059cb05564
Terry Thompson <terrylthompsonintx@gmail.com>
2020-05-28 15:10:08 -0500

    added post route and validation

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals');
 
 
@@ -47,6 +53,16 @@ const { animals } = require('./data/animals');
         const result = animalsArray.filter(animal => animal.id === id)[0];
         return result;
       }
+      function createNewAnimal(body, animalsArray) {
+        const animal = body;
+        animalsArray.push(animal);
+        fs.writeFileSync(
+          path.join(__dirname, './data/animals.json'),
+          JSON.stringify({ animals: animalsArray }, null, 2)
+        );
+        return animal;
+      }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -62,6 +78,15 @@ app.get('/api/animals', (req, res) => {
     } else {
       res.send(404);
     }
+  });
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+  
+    res.json(animal);
   });
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
