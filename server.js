b75d76ad6c633aa2ccdcbf69c4e056a65134053e
Terry Thompson <terrylthompsonintx@gmail.com>
2020-05-28 14:21:30 -0500

    added route

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ const { animals } = require('./data/animals');
         return filteredResults;
       }
   
-
+      function findById(id, animalsArray) {
+        const result = animalsArray.filter(animal => animal.id === id)[0];
+        return result;
+      }
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -51,6 +54,14 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
       }
   res.json(results);
+  });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
